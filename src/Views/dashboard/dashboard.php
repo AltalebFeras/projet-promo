0ac6de271386b5d5c1b50d9b564e176e38c7fe8d
Feras Altaleb <feras.altalib@gmail.com>
2024-08-20 13:26:36 +0200

    bd

--- src/Views/dashboard/dashboard.php
+++ src/Views/dashboard/dashboard.php
@@ -4,12 +4,26 @@
 
     <p>Hello from dashboard</p>
     <?php var_dump($_SESSION); ?>
-    <p>Bonjour <?php  echo $_SESSION['nom']?></p>
-    <?php if ($_SESSION['role'] === 'conducteur'): ?>
-    <p>ok</p>
-<?php else: ?>
-    <p>ko</p>
-<?php endif; ?>
+    <p>Bonjour <?php echo $_SESSION['nom'] ?></p>
+        <a href=""></a>
+    <?php
+    if ($_SESSION['role'] === 'admin'):
+
+        include_once __DIR__ . '/admin.php';
+
+    elseif ($_SESSION['role'] === 'conducteur'):
+
+        include_once __DIR__ . '/conducteur.php';
+
+    elseif ($_SESSION['role'] === 'mecanicien'):
+
+        include_once __DIR__ . '/mecanicien.php';
+
+    else:
+    echo 'Error - Please sign out';
+    endif;
+    ?>
+
 
 </div>
 
