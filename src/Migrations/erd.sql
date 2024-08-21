df56d9910d45d2fd49b82fe66b267725ae056acc
Feras Altaleb <feras.altalib@gmail.com>
2024-08-21 15:26:13 +0200

    modification database

--- src/Migrations/erd.sql
+++ src/Migrations/erd.sql
@@ -9,12 +9,18 @@ CREATE TABLE transport_commentaires
   PRIMARY KEY (Id_commentaire)
 );
 
+ALTER TABLE transport_commentaires
+  ADD CONSTRAINT UQ_Id_commentaire UNIQUE (Id_commentaire);
+
 CREATE TABLE transport_etat
 (
   Id_etat_vehicule INT          NOT NULL AUTO_INCREMENT,
   nom              VARCHAR(150) NOT NULL,
   PRIMARY KEY (Id_etat_vehicule)
 );
+
+ALTER TABLE transport_etat
+  ADD CONSTRAINT UQ_Id_etat_vehicule UNIQUE (Id_etat_vehicule);
 
 CREATE TABLE transport_evaluations
 (
@@ -26,20 +32,28 @@ CREATE TABLE transport_evaluations
   PRIMARY KEY (Id_evaluation)
 );
 
+ALTER TABLE transport_evaluations
+  ADD CONSTRAINT UQ_Id_evaluation UNIQUE (Id_evaluation);
+
 CREATE TABLE transport_personnels
 (
-  Id_personnel         INT          NOT NULL AUTO_INCREMENT,
-  nom                  VARCHAR(50)  NOT NULL,
-  prenom               VARCHAR(50)  NOT NULL,
-  date_arrive          DATE         NOT NULL,
-  telephone            INT          NOT NULL,
-  email                VARCHAR(150) NOT NULL,
-  mdp                  VARCHAR(200) NOT NULL,
-  dtc                  DATETIME     NOT NULL,
-  Id_statut_personnels INT          NOT NULL,
-  Id_role              INT          NOT NULL,
+  Id_personnel INT          NOT NULL AUTO_INCREMENT,
+  nom          VARCHAR(50)  NOT NULL,
+  prenom       VARCHAR(50)  NOT NULL,
+  date_arrive  DATE         NOT NULL,
+  telephone    INT          NOT NULL,
+  email        VARCHAR(150) NOT NULL,
+  mdp          VARCHAR(200) NOT NULL,
+  dtc          DATETIME     NOT NULL,
+  Id_role      INT          NOT NULL,
   PRIMARY KEY (Id_personnel)
 );
+
+ALTER TABLE transport_personnels
+  ADD CONSTRAINT UQ_Id_personnel UNIQUE (Id_personnel);
+
+ALTER TABLE transport_personnels
+  ADD CONSTRAINT UQ_email UNIQUE (email);
 
 CREATE TABLE transport_roles
 (
@@ -48,14 +62,31 @@ CREATE TABLE transport_roles
   PRIMARY KEY (Id_role)
 );
 
+ALTER TABLE transport_roles
+  ADD CONSTRAINT UQ_Id_role UNIQUE (Id_role);
+
 CREATE TABLE transport_statut
 (
-  Id_statut_personnels INT          NOT NULL AUTO_INCREMENT,
-  nom                  VARCHAR(150) NOT NULL,
-  date_debut           DATE         NULL    ,
-  date_fin             DATE         NULL    ,
+  Id_status INT          NOT NULL AUTO_INCREMENT,
+  nom       VARCHAR(150) NOT NULL,
+  PRIMARY KEY (Id_status)
+);
+
+ALTER TABLE transport_statut
+  ADD CONSTRAINT UQ_Id_status UNIQUE (Id_status);
+
+CREATE TABLE transport_statut_personnel
+(
+  Id_statut_personnels INT  NOT NULL AUTO_INCREMENT,
+  Id_status            INT  NOT NULL,
+  Id_personnel         INT  NOT NULL,
+  date_debut           DATE NULL    ,
+  date_fin             DATE NULL    ,
   PRIMARY KEY (Id_statut_personnels)
 );
+
+ALTER TABLE transport_statut_personnel
+  ADD CONSTRAINT UQ_Id_statut_personnels UNIQUE (Id_statut_personnels);
 
 CREATE TABLE transport_vehicules
 (
@@ -68,10 +99,8 @@ CREATE TABLE transport_vehicules
   PRIMARY KEY (Id_vehicule)
 );
 
-ALTER TABLE transport_personnels
-  ADD CONSTRAINT FK_transport_statut_TO_transport_personnels
-    FOREIGN KEY (Id_statut_personnels)
-    REFERENCES transport_statut (Id_statut_personnels);
+ALTER TABLE transport_vehicules
+  ADD CONSTRAINT UQ_Id_vehicule UNIQUE (Id_vehicule);
 
 ALTER TABLE transport_personnels
   ADD CONSTRAINT FK_transport_roles_TO_transport_personnels
@@ -100,5 +129,15 @@ ALTER TABLE transport_evaluations
 
 ALTER TABLE transport_evaluations
   ADD CONSTRAINT FK_transport_personnels_TO_transport_evaluations1
+    FOREIGN KEY (Id_personnel)
+    REFERENCES transport_personnels (Id_personnel);
+
+ALTER TABLE transport_statut_personnel
+  ADD CONSTRAINT FK_transport_statut_TO_transport_statut_personnel
+    FOREIGN KEY (Id_status)
+    REFERENCES transport_statut (Id_status);
+
+ALTER TABLE transport_statut_personnel
+  ADD CONSTRAINT FK_transport_personnels_TO_transport_statut_personnel
     FOREIGN KEY (Id_personnel)
     REFERENCES transport_personnels (Id_personnel);
--- src/Views/dashboard/personnel_detaille.php
+++ src/Views/dashboard/personnel_detaille.php
@@ -1,20 +1,60 @@
-    <p>page personnel detaille</p>
+<?php include_once __DIR__ . '/../includes/header.php'; ?>
 
- 
-    <h1>Personnel Details</h1>
-    <p><strong>Name:</strong> <?= htmlspecialchars($personnel['nom']) ?> <?= htmlspecialchars($personnel['prenom']) ?></p>
-    <p><strong>Email:</strong> <?= htmlspecialchars($personnel['email']) ?></p>
-    <p><strong>Telephone:</strong> <?= htmlspecialchars($personnel['telephone']) ?></p>
-    <p><strong>Role:</strong> <?= htmlspecialchars($personnel['role_name']) ?></p>
-    <p><strong>Status:</strong> <?= htmlspecialchars($personnel['statut_personnels']) ?></p>
-    <p><strong>Last Evaluation:</strong> <?= isset($personnel['evaluation']) ? htmlspecialchars($personnel['evaluation']) : 'No evaluation available' ?></p>
 
-    <form method="post" action="<?= HOME_URL . 'dashboard/personnel_detaille?Id_personnel=' . $personnel['Id_personnel'] ?>">
-    <input type="hidden" name="Id_admin" value="<?= $_SESSION['Id_personnel'] ?>"/>
-    <input type="hidden" name="Id_personnel" value="<?= $_GET['Id_personnel'] ?>"/>
-    <input type="hidden" name="action" value="ajouter_evaluation"/> <!-- Hidden action field -->
-    <label for="texte">Ajouter une évaluation:</label>
-    <input type="text" name="texte" id="texte"/>
-    <button type="submit">Soumettre</button>
+<p>page personnel detaille</p>
+
+
+<h1>Personnel Details</h1>
+
+<form action="" method="post">
+    <label for="nom">Nom</label>
+    <input type="text" id="nom" name="nom" value="<?= htmlspecialchars($personnel['nom']) ?>"><br>
+    <label for="prenom">Prénom</label>
+    <input type="text" id="prenom" name="prenom" value="<?= htmlspecialchars($personnel['prenom']) ?>"><br>
+
+    <label for="date_arrive">Date d'arrivée</label>
+    <input type="date" id="date_arrive" name="date_arrive" value="<?= htmlspecialchars($personnel['date_arrive']) ?>"><br>
+
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" value="<?= htmlspecialchars($personnel['email']) ?>"><br>
+    <label for="telephone">Téléphone</label>
+    <input type="text" id="telephone" name="telephone" value="<?= htmlspecialchars($personnel['telephone']) ?>"><br>
+    
+    <button type="submit">Modifier</button>
 </form>
 
+<p>Le personnel est actuellement : <?= htmlspecialchars($personnel['statut_personnels']) ?></p>
+<p>Déclarer un changement ?</p>
+
+<form action="">
+    <select id="statut" name="statut">
+        <option value="">Selectionnez un statut</option>
+        <?php foreach ($statuts as $statut) : ?>
+            <option value="<?= $statut['Id_statut_personnels'] ?>"><?= $statut['statut_personnels'] ?>
+        </option>
+        <?php endforeach; ?>
+    </select>
+
+    <button onclick="changeStatut(<?= $personnel['Id_personnel'] ?>)">Changer le statut</button>
+</form>
+
+
+<p>Ajouter une évaluation :</p>
+
+
+<form method="post" action="<?= HOME_URL . 'dashboard/personnel_detaille?Id_personnel=' . $personnel['Id_personnel'] ?>">
+    <input type="hidden" name="Id_admin" value="<?= $_SESSION['Id_personnel'] ?>" />
+    <input type="hidden" name="Id_personnel" value="<?= $_GET['Id_personnel'] ?>" />
+    <input type="hidden" name="action" value="ajouter_evaluation" /> <!-- Hidden action field -->
+    <label for="texte">Ajouter une évaluation:</label>
+    <input type="text" name="texte" id="texte" />
+    <button type="submit">Soumettre</button>
+</form>
+<p><strong>Last Evaluation:</strong> <?= isset($personnel['evaluation']) ? htmlspecialchars($personnel['evaluation']) : 'No evaluation available' ?></p>
+
+<p>Effacer ce personnel ?</p>
+<form action="">
+    <button onclick="deletePersonnel(<?= $personnel['Id_personnel'] ?>)"><i class="bi bi-trash"></i></button>
+</form>
+
+<?php include_once __DIR__ . '/../includes/footer.php'; ?>
