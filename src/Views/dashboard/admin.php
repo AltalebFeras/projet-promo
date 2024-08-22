55287ebd3b2fa61cd31f307c5b6bea42e22b8ed8
Sonia <sonia.blankenmeister@gmail.com>
2024-08-22 15:05:17 +0200

    admin: ajout css pour l'admin, changement du foreach, rajout des font dans le header

--- src/Views/dashboard/admin.php
+++ src/Views/dashboard/admin.php
@@ -1,8 +1,40 @@
-<div class="container mt-5">
-    <div class="d-flex justify-content-between align-items-center mb-4">
-        <img class="logo-admin" src="<?= Domain . HOME_URL . 'assets/image/Logo.png'?>" alt="Logo de l'entreprise" />
-        <a class="btn btn-success rounded-pill" href="<?= Domain . HOME_URL ?>dashboard/ajouter_personnel">Ajouter Personnel</a>
+<div class="container">
+    <div class="row align-items-center mb-5">
+        
+        <div class="col-md-3">
+            <img class="logo-admin" src="<?= Domain . HOME_URL . 'assets/image/Logo.png'?>" alt="Logo de l'entreprise" />
+        </div>
+
+        <div class="col-md-3">
+            <a class="btn btn-bg-color rounded-pill" href="<?= Domain . HOME_URL ?>dashboard/ajouter_personnel">Ajouter Personnel</a>
+        </div>
+
+        <div class="col-md-3">
+            <div class="d-flex">
+                <p class="mb-0 me-2">Trier par type:</p>
+                <select class="form-select w-auto">
+                <option selected class="bg-warning">Choisir ici</option>
+                <option value="1">Administrateur</option>
+                <option value="2">Conducteur</option>
+                <option value="3">Mécanicien</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="d-flex">
+                <p class="mb-0 me-2">Trier par type:</p>
+            <select class="form-select w-auto">
+                <option selected>Choisir ici</option>
+                <option value="1">Administrateur</option>
+                <option value="2">Conducteur</option>
+                <option value="3">Mécanicien</option>
+            </select>
+            </div>
+        </div>
+
     </div>
+    
 
     <div class="alert-container">
         <?php if (isset($_GET['error'])): ?>
@@ -13,27 +45,19 @@
         <?php endif; ?>
     </div>
 
-    <div class="d-flex mb-4">
-        <p class="mb-0 me-2">Trier par type:</p>
-        <select class="form-select w-auto">
-            <option selected>Choisir ici</option>
-            <option value="1">Administrateur</option>
-            <option value="2">Conducteur</option>
-            <option value="3">Mécanicien</option>
-        </select>
-    </div>
+    
 
     <div class="row">
-        <?php foreach ($personnels as $personnel) : ?>
+        <?php foreach ($allPersonnelsWithStatus  as $personnel) : ?>
             <div class="col-md-4 mb-4">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title"><?= htmlspecialchars($personnel['nom']) . ' ' . htmlspecialchars($personnel['prenom']) ?></h5>
-                        <p class="card-text"><strong>Rôle:</strong> <?= htmlspecialchars($personnel['role_name']) ?></p>
-                        <p class="card-text"><strong>Email:</strong> <?= htmlspecialchars($personnel['email']) ?></p>
-                        <p class="card-text"><strong>Téléphone:</strong> <?= htmlspecialchars($personnel['telephone']) ?></p>
-                        <p><strong>Statut du personnel:</strong>  </p>
-                        <p class="card-text"><strong>Évaluation:</strong> <?= isset($personnel['last_evaluation']) ? htmlspecialchars($personnel['last_evaluation']) : 'Aucune évaluation' ?></p>
+                        <h5 class="card-title"><?= $personnel['nom'] . ' ' . $personnel['prenom'] ?></h5>
+                        <p class="card-text"><strong>Rôle:</strong> <?= $personnel['role_name'] ?></p>
+                        <p class="card-text"><strong>Email:</strong> <?= $personnel['email'] ?></p>
+                        <p class="card-text"><strong>Téléphone:</strong> <?= $personnel['telephone'] ?></p>
+                        <p><strong>Statut du personnel:</strong> <?= $personnel['status_name'] ?> </p>
+                        <p class="card-text"><strong>Évaluation:</strong> <?= isset($personnel['last_evaluation']) ? $personnel['last_evaluation'] : 'Aucune évaluation' ?></p>
                         <a href="<?= Domain . HOME_URL . 'dashboard/personnel_detaille?Id_personnel=' . $personnel['Id_personnel'] ?>" class="btn btn-primary">Détails</a>
                     </div>
                 </div>
